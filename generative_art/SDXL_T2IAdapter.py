9818820a97081aca344f473744dc042fbc8779ca
Juyeon Olivia Song <116419558+j-o-song@users.noreply.github.com>
2024-05-09 09:41:30 -0400

    Update SDXL_T2IAdapter.py

--- generative_art/SDXL_T2IAdapter.py
+++ generative_art/SDXL_T2IAdapter.py
@@ -14,6 +14,7 @@ pipeline.to("mps")
 
 generator = torch.Generator(device="mps").manual_seed(43)
 
+#Image Generation Parameters 
 negative_prompt = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature"
 input_image = ImageOps.invert(load_image("Sketches/mthouse.jpg").resize((512, 512)))
 prompt = "A realistic photo of a wooden house in the alps"
